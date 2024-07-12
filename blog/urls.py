9bf7d82e3bbae9eb4a97772ff053b458b441c984
KleberAraujo <kleberson.bezerra@hotmail.com>
2024-07-12 09:27:48 -0300

    correção named

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, named='post_list' ),
+    path('', views.post_list, name='post_list' ),
     #path('',views.porta), # Com aspas vazia '' entramos na parte inicial do site
     path('sala', views.sala), # Com aspas preenchida entramos no site juntamente com a /sala
     path('quarto', views.quarto) # Mesma coisa nesta linha
